cd547b96c29a5150ed40c90d611bd063d8a756d0
SubburamK <subburam@e2o.in>
2026-02-06 16:16:14 +0530

    complete the corrections

--- src/components/Contact/Form/index.tsx
+++ src/components/Contact/Form/index.tsx
@@ -11,73 +11,111 @@ const ContactForm = () => {
                     <div className="grid md:grid-cols-12 grid-cols-1 gap-8">
                         <div className="col-span-6">
                             <h2 className="max-w-72 text-[40px] leading-[3.4rem] font-bold mb-9 text-secondary">Get Online Consultation</h2>
-                            <form className="flex flex-wrap w-full m-auto justify-between">
-                                <div className="sm:flex gap-3 w-full">
-                                    <div className="mx-0 my-2.5 flex-1">
-                                        <label htmlFor="first-name" className="pb-3 inline-block text-base text-SlateBlueText">First Name*</label>
-                                        <input
-                                            id="first-name"
-                                            className="w-full text-base px-4 rounded-lg py-2.5 border-border dark:border-dark_border border-solid dark:text-white  dark:bg-darkmode border transition-all duration-500 focus:border-primary dark:focus:border-primary focus:border-solid focus:outline-0"
-                                            type="text"
-                                        />
-                                    </div>
-                                    <div className="mx-0 my-2.5 flex-1">
-                                        <label htmlFor="last-name" className="pb-3 inline-block text-base text-SlateBlueText">Last Name*</label>
-                                        <input
-                                            id="last-name"
-                                            className="w-full text-base px-4 py-2.5 rounded-lg border-border dark:border-dark_border border-solid dark:text-white  dark:bg-darkmode border transition-all duration-500 focus:border-primary dark:focus:border-primary focus:border-solid focus:outline-0"
-                                            type="text"
-                                        />
-                                    </div>
-                                </div>
-                                <div className="sm:flex gap-3 w-full">
-                                    <div className="mx-0 my-2.5 flex-1">
-                                        <label htmlFor="email" className="pb-3 inline-block text-base text-SlateBlueText">Email address*</label>
-                                        <input
-                                            id="email"
-                                            type="email"
-                                            className="w-full text-base px-4 py-2.5 rounded-lg border-border dark:border-dark_border border-solid dark:text-white  dark:bg-darkmode border transition-all duration-500 focus:border-primary dark:focus:border-primary focus:border-solid focus:outline-0"
-                                        />
-                                    </div>
-                                    <div className="mx-0 my-2.5 flex-1">
-                                        <label htmlFor="Specialist" className="pb-3 inline-block text-base text-SlateBlueText">Specialist*</label>
-                                        <select id="Specialist" className="w-full text-base px-4 py-2.5 text-SlateBlueText rounded-lg border-border dark:text-white border-solid dark:bg-darkmode border transition-all duration-500 focus:border-primary dark:focus:border-primary dark:border-dark_border focus:border-solid focus:outline-0">
-                                            <option value="">Choose a specialist</option>
-                                            <option value="Baking &amp; Pastry">
-                                                Choose a specialist
-                                            </option>
-                                            <option value="Exotic Cuisine">Exotic Cuisine</option>
-                                            <option value="French Desserts">French Desserts</option>
-                                            <option value="Seafood &amp; Wine">
-                                                Choose a specialist
-                                            </option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div className="sm:flex gap-3 w-full">
-                                    <div className="mx-0 my-2.5 flex-1">
-                                        <label htmlFor="date" className="pb-3 inline-block text-base text-SlateBlueText">Date*</label>
-                                        <input
-                                            id="date"
-                                            className="w-full text-base text-SlateBlueText px-4 rounded-lg py-2.5 outline-none dark:text-white dark:bg-darkmode border-border border-solid border transition-all duration-500 focus:border-primary dark:focus:border-primary dark:border-dark_border focus:border-solid focus:outline-0"
-                                            type="date"
-                                        />
-                                    </div>
-                                    <div className="mx-0 my-2.5 flex-1">
-                                        <label htmlFor="time" className="pb-3 inline-block text-base text-SlateBlueText">Time*</label>
-                                        <input
-                                            id="time"
-                                            className="w-full text-base px-4 rounded-lg py-2.5 border-border outline-none dark:text-white dark:bg-darkmode border-solid border transition-all duration-500 focus:border-primary dark:focus:border-primary dark:border-dark_border focus:border-solid focus:outline-0"
-                                            type="time"
-                                        />
-                                    </div>
-                                </div>
-                                <div className="mx-0 my-2.5 w-full">
-                                    <Link href="#" className="mt-4 btn btn-1 hover-filled-slide-down overflow-hidden rounded-lg" type="submit">
-                                        <span>Make an appointment</span>
-                                    </Link>
-                                </div>
-                            </form>
+                          <form className="flex flex-wrap w-full m-auto justify-between">
+
+  {/* NAME */}
+  <div className="sm:flex gap-3 w-full">
+    <div className="mx-0 my-2.5 flex-1">
+      <label className="pb-3 inline-block text-base text-SlateBlueText">
+        Name*
+      </label>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        className="w-full text-base px-4 py-2.5 rounded-lg
+        border border-border dark:border-dark_border
+        dark:text-white dark:bg-darkmode
+        transition-all duration-500
+        focus:border-primary focus:outline-0"
+      />
+    </div>
+  </div>
+
+  {/* EMAIL */}
+  <div className="sm:flex gap-3 w-full">
+    <div className="mx-0 my-2.5 flex-1">
+      <label className="pb-3 inline-block text-base text-SlateBlueText">
+        Email Address*
+      </label>
+      <input
+        type="email"
+        placeholder="Enter your email"
+        className="w-full text-base px-4 py-2.5 rounded-lg
+        border border-border dark:border-dark_border
+        dark:text-white dark:bg-darkmode
+        transition-all duration-500
+        focus:border-primary focus:outline-0"
+      />
+    </div>
+  </div>
+
+  {/* PHONE */}
+  <div className="sm:flex gap-3 w-full">
+    <div className="mx-0 my-2.5 flex-1">
+      <label className="pb-3 inline-block text-base text-SlateBlueText">
+        Phone Number*
+      </label>
+      <input
+        type="tel"
+        placeholder="Enter your phone number"
+        className="w-full text-base px-4 py-2.5 rounded-lg
+        border border-border dark:border-dark_border
+        dark:text-white dark:bg-darkmode
+        transition-all duration-500
+        focus:border-primary focus:outline-0"
+      />
+    </div>
+  </div>
+
+  {/* LOCATION */}
+  <div className="sm:flex gap-3 w-full">
+    <div className="mx-0 my-2.5 flex-1">
+      <label className="pb-3 inline-block text-base text-SlateBlueText">
+        Location*
+      </label>
+      <input
+        type="text"
+        placeholder="Enter your location"
+        className="w-full text-base px-4 py-2.5 rounded-lg
+        border border-border dark:border-dark_border
+        dark:text-white dark:bg-darkmode
+        transition-all duration-500
+        focus:border-primary focus:outline-0"
+      />
+    </div>
+  </div>
+
+  {/* SERVICE */}
+  <div className="sm:flex gap-3 w-full">
+    <div className="mx-0 my-2.5 flex-1">
+      <label className="pb-3 inline-block text-base text-SlateBlueText">
+        Service*
+      </label>
+      <input
+        type="text"
+        placeholder="Enter required service"
+        className="w-full text-base px-4 py-2.5 rounded-lg
+        border border-border dark:border-dark_border
+        dark:text-white dark:bg-darkmode
+        transition-all duration-500
+        focus:border-primary focus:outline-0"
+      />
+    </div>
+  </div>
+
+  {/* BUTTON */}
+  <div className="mx-0 my-4 w-full">
+    <button
+      type="submit"
+      className="mt-4 btn btn-1 hover-filled-slide-down
+      overflow-hidden rounded-lg w-full"
+    >
+      <span>Contact Us</span>
+    </button>
+  </div>
+
+</form>
+
                         </div>
                         <div className="col-span-6">
                             <Image
